edc867b860da9efdea504063c9349022e3b6b9cf
Sunnee Chevalier <168848215+sunneec-croesus@users.noreply.github.com>
2024-07-09 11:51:38 -0400

    feat: changed workflows to use croesusfin actions instead of custom

--- .commitlintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-angular'],
-  rules: {
-    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'style', 'test', 'release']],
-  },
-};
